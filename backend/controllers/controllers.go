5c44c61a4b94853475d8cf4b9d546f93b000624d
shamim-yazdani <shamimyazdani98@gmail.com>
2024-10-03 00:13:48 -0500

    controller

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -1,1 +1,34 @@
 package controllers
+
+// not tested
+// import (
+// 	"net/http"
+// 	"github.com/gin-gonic/gin"
+// 	"project/models"  
+// )
+
+func GetUser(c *gin.Context) {
+	id := c.Param("id")  // Get the user ID from the URL
+	user, found := models.GetUserByID(id)
+
+	if !found {
+		c.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+
+	
+	c.JSON(http.StatusOK, user)
+}
+
+func CreateUser(c *gin.Context) {
+	var newUser models.User
+
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.CreateUser(newUser)
+
+	c.JSON(http.StatusCreated, newUser)
+}
