48e8c7a5f79bee86d498625844a89ad8a2f7904c
abhishekghimire40 <abhishekghimire40@gmail.com>
2024-10-16 21:43:26 -0500

    create a new route /auth/refresh-token to generate access token from refresh token and its handler function
    created a new route /auth/refresh-token to generate access token from
    refresh token and also updated authentication function to authenticate
    only with access tokens and refactored my token gen functions into
    smaller functions

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -12,12 +12,17 @@ func UserRoutes(incomingRoutes *gin.Engine, db *database.Queries) {
 	incomingRoutes.GET("/category", controllers.GetAllCategory(db))
 	incomingRoutes.POST("/users/signup", controllers.Signup(db))
 	incomingRoutes.POST("/users/login", controllers.Login(db))
+	incomingRoutes.POST("/auth/refresh-token", controllers.GenerateAccessTokenFromRefreshToken(db))
 	authRoutes := incomingRoutes.Group("/")
 	authRoutes.Use(middleware.Authentication())
 	{
+		// auth route
+
+		// user route
 		authRoutes.POST("/users/transactions", controllers.AddTransaction(db))
 		authRoutes.GET("/users/transactions", controllers.GetTransactions(db))
 		authRoutes.DELETE("/users/transactions/:id", controllers.DeleteTransactions(db))
+
 	}
 
 	adminRoutes := incomingRoutes.Group("/admin")
--- backend/tokens/tokengen.go
+++ backend/tokens/tokengen.go
@@ -1,7 +1,6 @@
 package tokens
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -9,6 +8,7 @@ import (
 )
 
 type SignedDetails struct {
+	Token_Type string
 	Email      string
 	First_Name string
 	Last_Name  string
@@ -20,37 +20,67 @@ type SignedDetails struct {
 
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
 
-func TokenGenerator(
-	email, first_name, last_name, username, uid, role string,
-) (string, string, error) {
-	claims := &SignedDetails{
+func generateClaims(
+	email, first_name, last_name, username, uid, role, token string,
+) *SignedDetails {
+	var expiring_time time.Time
+	switch token {
+	case "access":
+		expiring_time = time.Now().Local().Add(time.Minute * 30)
+	case "refresh":
+		expiring_time = time.Now().Local().Add(time.Hour * 24)
+	default:
+		expiring_time = time.Now().Local().Add(time.Hour)
+	}
+
+	return &SignedDetails{
+		Token_Type: token,
 		Email:      email,
+		Username:   username,
 		First_Name: first_name,
 		Last_Name:  last_name,
-		Username:   username,
 		Uid:        uid,
 		Role:       role,
 		RegisteredClaims: jwt.RegisteredClaims{
 			Subject:   username,
 			Issuer:    "finance-tracker",
-			ExpiresAt: jwt.NewNumericDate(time.Now().Local().Add(time.Hour * 2)),
+			ExpiresAt: jwt.NewNumericDate(expiring_time),
 		},
 	}
+}
 
-	refreshClaims := &SignedDetails{
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Local().Add(time.Hour * 24)),
-		},
-	}
-
-	fmt.Print(SECRET_KEY)
+// function that generates access token
+func GenerateAccessToken(email, first_name, last_name, username, uid, role string) (string, error) {
+	claims := generateClaims(email, first_name, last_name, username, uid, role, "access")
 	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+// function that generates refresh token
+func GenerateRefreshToken(
+	email, first_name, last_name, username, uid, role string,
+) (string, error) {
+	claims := generateClaims(email, first_name, last_name, username, uid, role, "refresh")
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
+// function that generates both the access and refresh token at the same time
+func TokenGenerator(
+	email, first_name, last_name, username, uid, role string,
+) (string, string, error) {
+	token, err := GenerateAccessToken(email, first_name, last_name, username, uid, role)
 	if err != nil {
 		return "", "", err
 	}
 
-	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).
-		SignedString([]byte(SECRET_KEY))
+	refreshToken, err := GenerateRefreshToken(email, first_name, last_name, username, uid, role)
 	if err != nil {
 		return "", "", err
 	}
@@ -58,7 +88,8 @@ func TokenGenerator(
 	return token, refreshToken, nil
 }
 
-func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+// function that helps to retrieve claims from token
+func GetClaims(signedToken string) (claims *SignedDetails, msg string) {
 	token, err := jwt.ParseWithClaims(
 		signedToken,
 		&SignedDetails{},
@@ -74,6 +105,15 @@ func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
 	claims, ok := token.Claims.(*SignedDetails)
 	if !ok {
 		msg = "the token is invalid"
+		return nil, msg
+	}
+	return claims, ""
+}
+
+// function to validate access and refresh tokens
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	claims, msg = GetClaims(signedToken)
+	if msg != "" {
 		return nil, msg
 	}
 
