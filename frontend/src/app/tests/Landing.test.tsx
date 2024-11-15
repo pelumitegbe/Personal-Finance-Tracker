d3eb47073694df1d1facc154ec68a0cc3fd33d36
pelumitegbe <pelumiayomikunoluwategbe@gmail.com>
2024-11-14 19:01:21 -0600

    Changes to landing test page

--- frontend/src/app/tests/Landing.test.tsx
+++ frontend/src/app/tests/Landing.test.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Landing from "../page.tsx";
-import TopNavigation from "../components/TopNavigation";
 
 // Mock TopNavigation component
 jest.mock("../components/TopNavigation", () => {
