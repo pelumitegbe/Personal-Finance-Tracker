ec7c4de2f7aa51e5ee45e89dd0d8eeb61117642a
Pelumi Oluwategbe <pelumitegbe@gmail.com>
2024-11-22 23:22:30 -0600

    changes made to the frontend testing

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,11 +2,15 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    "^.+\\.(ts|tsx|js|jsx)$": "babel-jest"
   },
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/$1"
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: [
+    "/node_modules/"
+  ],
+  extensionsToTreatAsEsm: [".ts", ".tsx"]
 };
