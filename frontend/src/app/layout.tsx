d25425fd61dc799aae16967edc80a8768d71fdf4
Pelumi Oluwategbe <pelumitegbe@gmail.com>
2024-12-06 16:19:25 -0600

    changes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,12 @@
 "use client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import "./globals.css";
+import React from "react";
 // import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import AuthContextProvider from "./context";
 import { queryClient } from "./react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
 //   title: "Finance Tracker",
@@ -16,14 +14,14 @@ const inter = Inter({ subsets: ["latin"] });
 // };
 
 export default function RootLayout({ children }: { children: JSX.Element }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          <ToastContainer />
-          <AuthContextProvider>{children}</AuthContextProvider>
-        </QueryClientProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className='inter'>
+				<QueryClientProvider client={queryClient}>
+					<ToastContainer />
+					<AuthContextProvider>{children}</AuthContextProvider>
+				</QueryClientProvider>
+			</body>
+		</html>
+	);
 }
